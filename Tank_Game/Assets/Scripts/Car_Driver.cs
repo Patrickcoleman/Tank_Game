c5fcffe0555cc2b415f74f9ee3bc53b9c6fb8a50
Patrick Coleman <sirpatrickcoleman@gmail.com>
2023-06-27 23:34:12 +1000

    fixed gun rotation broke driving

--- Tank_Game/Assets/Scripts/Car_Driver.cs
+++ Tank_Game/Assets/Scripts/Car_Driver.cs
@@ -8,6 +8,8 @@ public class Car_Driver : NetworkBehaviour
     public GameObject Tank_Skin;
     public GameObject Gun_Skin;
     public float currentSpeed = 0;
+    public float movespeed = 10;
+    public float turnspeed = 100;
     private Plane _background = new(Vector3.back, Vector3.zero);
     private Camera _cam;
 
@@ -42,7 +44,7 @@ public class Car_Driver : NetworkBehaviour
             mouse_pos.x = object_pos.x - mouse_pos.x;
             mouse_pos.y = object_pos.y - mouse_pos.y;
             angle = Mathf.Atan2(mouse_pos.y, mouse_pos.x) * Mathf.Rad2Deg - 90;
-            Gun_Skin.transform.rotation = Quaternion.Euler(0, 0, angle);
+            Gun_Skin.transform.rotation = Quaternion.RotateTowards(Gun_Skin.transform.rotation, Quaternion.Euler(0, 0, angle), 3);;
 
 
         // // Update rotation of Gun
@@ -58,26 +60,26 @@ public class Car_Driver : NetworkBehaviour
 
         // Debug.Log(Input.mousePosition);
         //
-        //currentSpeed = Mathf.Round((currentSpeed * 50000 * Time.deltaTime) * 0.95f)/100;
-        currentSpeed = 20;
+        currentSpeed = Mathf.Round((currentSpeed * 100 * Time.deltaTime) * 0.95f)/100;
+        //currentSpeed = 0;
         var rotationVector = Tank_Skin.transform.rotation.eulerAngles;
-        this.transform.position = this.transform.position + new Vector3(Mathf.Sin(rotationVector.z * Mathf.PI/180) * Time.deltaTime * currentSpeed * 0.05f,-Mathf.Cos(rotationVector.z * Mathf.PI/180) * Time.deltaTime * currentSpeed * 0.05f,0);
+        this.transform.position = this.transform.position + new Vector3(Mathf.Sin(rotationVector.z * Mathf.PI/180) * Time.deltaTime * currentSpeed,-Mathf.Cos(rotationVector.z * Mathf.PI/180) * Time.deltaTime * currentSpeed,0);
         
         if(Input.GetKey(KeyCode.UpArrow)){
-            currentSpeed += 10;
+            currentSpeed += movespeed;
         }
 
         if(Input.GetKey(KeyCode.DownArrow)){
-            currentSpeed -= 10;
+            currentSpeed -= movespeed;
         }
 
         if(Input.GetKey(KeyCode.LeftArrow)){
-            rotationVector.z += Time.deltaTime * 100;
+            rotationVector.z += Time.deltaTime * turnspeed;
             Tank_Skin.transform.rotation = Quaternion.Euler(rotationVector);
         }
 
         if(Input.GetKey(KeyCode.RightArrow)){
-            rotationVector.z -= Time.deltaTime * 100;
+            rotationVector.z -= Time.deltaTime * turnspeed;
             Tank_Skin.transform.rotation = Quaternion.Euler(rotationVector);
         }
     }
